50e76339ce66bf58ff24048a850ab0cff6a03711
Anant mishra <anant3380@gmail.com>
2025-08-02 12:17:38 +0530

    Update StatisticsServlet.java

--- src/main/java/com/library/servlet/StatisticsServlet.java
+++ src/main/java/com/library/servlet/StatisticsServlet.java
@@ -18,7 +18,7 @@ public class StatisticsServlet extends HttpServlet {
         try {
             Class.forName("com.mysql.jdbc.Driver"); // Updated Driver
             Connection con = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/library", "root", "Anant@2004");
+                "jdbc:mysql://localhost:3306/library", "root", "'Enter your password'");
 
             // Fetch issued books
             PreparedStatement ps1 = con.prepareStatement("SELECT * FROM issue_book");

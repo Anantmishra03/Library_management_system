71a582dc4a70136cc3f181d78d423e08048487a7
Anant mishra <anant3380@gmail.com>
2025-08-07 19:14:01 +0530

    Update BookServlet.java

--- src/main/java/com/library/servlet/BookServlet.java
+++ src/main/java/com/library/servlet/BookServlet.java
@@ -24,7 +24,7 @@ public class BookServlet extends HttpServlet {
 
             // Connect to database
             Connection con = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/library", "root", "'Enter your password'");
+                "jdbc:mysql://localhost:3306/library", "root", "Anant@2004");
 
             // Insert query
             String query = "INSERT INTO new_book (book_id, book_name, publisher, price) VALUES (?, ?, ?, ?)";

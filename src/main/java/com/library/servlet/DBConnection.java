d564837396049f5544adb20ccaae49a549639c80
Anant mishra <anant3380@gmail.com>
2025-08-07 19:14:16 +0530

    Update DBConnection.java

--- src/main/java/com/library/servlet/DBConnection.java
+++ src/main/java/com/library/servlet/DBConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/library"; // your database
     private static final String USER = "root"; 
-    private static final String PASSWORD = "'Enter your password'"; 
+    private static final String PASSWORD = "Anant@2004'"; 
 
     public static Connection getConnection() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.jdbc.Driver"); // ✅ Recommended for MySQL 8+

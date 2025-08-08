3f65decd634ec9b1c51cd7dbf7ae7c6906798251
Anant mishra <anant3380@gmail.com>
2025-08-08 18:08:43 +0530

    Update StudentServlet.java

--- src/main/java/com/library/servlet/StudentServlet.java
+++ src/main/java/com/library/servlet/StudentServlet.java
@@ -33,7 +33,7 @@ public class StudentServlet extends HttpServlet {
 
             // Connect to database
             Connection con = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/library", "root", "Anant@2004");
+                    "jdbc:mysql://localhost:3306/library", "root", "'enter your password'");
 
             // Insert query
             String query = "INSERT INTO new_student (student_id, student_name, course, branch) VALUES (?, ?, ?, ?)";
